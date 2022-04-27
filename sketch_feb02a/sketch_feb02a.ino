a1e79fdcee5a3afe3e5b70e3bfa6f866c48a592e
Jaime500 <61094798+Jaime500@users.noreply.github.com>
2022-04-26 21:34:13 -0400

    fixed abs in arduino and misspelling in index.html

--- sketch_feb02a/sketch_feb02a.ino
+++ sketch_feb02a/sketch_feb02a.ino
@@ -71,9 +71,9 @@ void loop()                     // this runs over and over again forever
  Serial.print(" ");
  Serial.print(normalTempValue);
   
-  normalLightValue = (abs)normalLightValue;
-  normalHygrometerValue = (abs)normalHygrometerValue;
-  normalTempValue = (abs)normalTempValue;
+  normalLightValue = abs(normalLightValue);
+  normalHygrometerValue = abs(normalHygrometerValue);
+  normalTempValue = abs(normalTempValue);
   
 
 //  Serial.print(lightLow);   // Prints the value via the serial port
